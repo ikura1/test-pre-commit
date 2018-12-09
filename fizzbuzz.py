7f0b32ab1837998ab36c492e18b9a2d8d7b453da
ikura1 <ikura.lab@gmail.com>
2018-12-09 14:36:16 +0900

    refactor: delete int

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 def main():
     for i in range(1, 31):
-        print(f'{i}:', int(not (i % 3)) * 'Fizz' + int(not (i % 5)) * 'Buzz' or i)
+        print(f'{i}:', not(i % 3) * 'Fizz' + not(i % 5) * 'Buzz' or i)
 
 
 if __name__ == '__main__':
