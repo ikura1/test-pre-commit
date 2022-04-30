5265a15f757d0df41b8bd8d96798e1792f577149
ikura1 <ikura.lab@gmail.com>
2022-04-30 20:52:11 +0900

    feat: 悲しみのコードでコミット

--- hoge.py
+++ hoge.py
@@ -1,21 +1,14 @@
 import random
-
-import click
-
 from fizzbuzz import fizzbuzz as fb
+import click
+import os
 
 
 @click.command()
-@click.option(
-    "--num",
-    "-n",
-    default=lambda: random.randint(1, 100),
-    type=int,
-    help="Number of loop",
-)
+@click.option('--num','-n',default=lambda: random.randint(1, 100),type=int,help='Number of loop')
 def fizzbuzz(num: int):
     fb(num)
 
 
-if __name__ == "__main__":
-    fizzbuzz()
+if __name__=='__main__':
+  fizzbuzz()
